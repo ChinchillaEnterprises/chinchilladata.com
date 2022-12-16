0cb8ada6e01840cad5ac6aea481f5b5f906f82a6
Aragorn <rideforruin777@gmail.com>
2022-12-16 14:44:16 -0800

    visual vs dashboard

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,3 +13,33 @@ var s3 = new AWS.S3({
   apiVersion: "2006-03-01",
   params: { Bucket: bucketName }
 });
+
+document.getElementById("demo").onclick = function() {addFile()};
+
+function addFile() {
+  var files = document.getElementById("fileupload").files
+  var file = files[0];
+  var fileName = file.name;
+  var folderName = Math.floor(Math.random() * 1001);
+  var bucketKey = folderName + "/" + fileName;
+
+  // Use S3 ManagedUpload class as it supports multipart uploads
+  var upload = new AWS.S3.ManagedUpload({
+    params: {
+      Bucket: bucketName,
+      Key: bucketKey,
+      Body: file
+    }
+  });
+
+  var promise = upload.promise();
+
+  promise.then(
+    function(data) {
+      alert("Successfully uploaded file.");
+    },
+    function(err) {
+      return alert("There was an error uploading your file: ", err.message);
+    }
+  );
+};
