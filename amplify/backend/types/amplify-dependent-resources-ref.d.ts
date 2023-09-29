96d150585f340e1540ead63ed973639fd97235f9
feathars91 <abelino.chinchilla@icloud.com>
2023-09-29 12:08:44 -0500

    🚀feat(function) add chinchilladatacom4927b707 function ⚙️chore(dependencies) update AWS Lambda runtime to nodejs18.x

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,11 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "chinchilladatacom4927b707": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
